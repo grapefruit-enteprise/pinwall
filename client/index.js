cee02b3b8907656c6d43b5efd3baf05ab2a83f3c
Stephanie Velazquez <stephanievelazquez@Stephanies-MacBook-Pro-2.local>
2016-07-13 13:57:52 -0700

    [environment:client] install redux-form

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import routes from './config/routes';
 import ReduxThunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 import reducers from './reducers/root-reducer.js';
 
